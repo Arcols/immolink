4600b3b474865232a3ead012aa643f5d7ced23ef
indi-blateau <indi.blateau@etu.iut-tlse3.fr>
2024-11-29 15:09:11 +0100

    normalement c'est ok

--- src/main/java/ihm/ModelePageBienImmobilier.java
+++ src/main/java/ihm/ModelePageBienImmobilier.java
@@ -1,20 +1,35 @@
 package ihm;
 
-import java.awt.event.ActionEvent;
-
-import javax.swing.DefaultComboBoxModel;
-
-import modele.PageBienImmobilier;
+import DAO.jdbc.BienLouableDAO;
+import classes.BienLouable;
+import javax.swing.table.DefaultTableModel;
+import java.sql.SQLException;
+import java.util.List;
 
 public class ModelePageBienImmobilier {
-	private PageBienImmobilier page;
 
-	public ModelePageBienImmobilier(PageBienImmobilier page) {
-		this.page = page;
-	}
+	private static DefaultTableModel loadDataBienImmoToTable() throws SQLException {
+		// Liste des colonnes pour les biens immobiliers
+		String[] columnNames = {"Numéro Fiscal", "Adresse", "Ville", "Complément Adresse"};
 
-	public void handleVilleSelection(ActionEvent e) {
-		this.page.getChoixVille().setModel(new DefaultComboBoxModel<>(this.page.getMapVillesAdresses()
-				.get(this.page.getChoixVille().getSelectedItem()).toArray(new String[0])));
+		// Création du modèle de table
+		DefaultTableModel model = new DefaultTableModel(columnNames, 0); // `0` pour aucune ligne au départ
+
+		// Récupération des biens immobiliers
+		BienLouableDAO allBiens = new BienLouableDAO();
+		List<BienLouable> biens = allBiens.findAll();
+
+		// Remplissage du modèle avec les données des biens
+		for (BienLouable bien2 : biens) {
+			Object[] rowData = {
+					bien2.getNumero_fiscal(),
+					bien2.getAdresse(),
+					bien2.getVille(),
+					bien2.getComplement_adresse()
+			};
+			model.addRow(rowData); // Ajout de la ligne dans le modèle
+		}
+
+		return model;
 	}
 }
