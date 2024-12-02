8c569363e4f4ecf0dae93611346669adb2e3c375
Masterking <153213881+MasterkingStudio@users.noreply.github.com>
2024-12-02 16:32:13 +0100

    Rajout du bon header sur nouveauBail

--- src/main/java/modele/PageNouveauBail.java
+++ src/main/java/modele/PageNouveauBail.java
@@ -1,13 +1,13 @@
 package modele;
 
+import ihm.Charte;
+import ihm.Menu;
 import ihm.ModelePageNouveauBail;
+import ihm.ResizedImage;
 
-import java.awt.BorderLayout;
-import java.awt.Color;
-import java.awt.Cursor;
-import java.awt.EventQueue;
-import java.awt.FlowLayout;
-import java.awt.GridLayout;
+import java.awt.*;
+import java.awt.event.ComponentAdapter;
+import java.awt.event.ComponentEvent;
 
 import javax.swing.JButton;
 import javax.swing.JComboBox;
@@ -68,15 +68,16 @@ public class PageNouveauBail {
 
         this.frame = new JFrame();
         this.frame.setBounds(100, 100, 750, 400);
-        this.frame.getContentPane().setBackground(FOND.getCouleur());
+        this.frame.getContentPane().setBackground(Charte.FOND.getCouleur());
         this.frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        Menu m = new Menu(this.frame);
 
         // Panel d'entête pour le logo et le nom de l'appli
         JPanel entete = new JPanel();
         this.frame.getContentPane().add(entete, BorderLayout.NORTH);
         entete.setLayout(new BorderLayout(0, 0));
 
-        entete.setBackground(ENTETE.getCouleur());
+        entete.setBackground(Charte.ENTETE.getCouleur());
         entete.setBorder(new LineBorder(Color.BLACK, 2));
         // Label pour le logo (Image)
         this.logo = new JLabel("");
@@ -85,38 +86,42 @@ public class PageNouveauBail {
 
         entete.add(menu_bouttons, BorderLayout.CENTER);
         menu_bouttons.setLayout(new GridLayout(0, 5, 0, 0));
-        menu_bouttons.setBackground(ENTETE.getCouleur());
+        menu_bouttons.setBackground(Charte.ENTETE.getCouleur());
 
         JButton b_accueil = new JButton("Accueil");
         b_accueil.setBorderPainted(false);
-        b_accueil.setBackground(ENTETE.getCouleur());
+        b_accueil.setBackground(Charte.ENTETE.getCouleur());
         b_accueil.setCursor(new Cursor(Cursor.HAND_CURSOR));
         menu_bouttons.add(b_accueil);
+        b_accueil.addActionListener(m);
 
         JButton b_profil = new JButton("Profil");
         b_profil.setBorderPainted(false);
-        b_profil.setBackground(ENTETE.getCouleur());
+        b_profil.setBackground(Charte.ENTETE.getCouleur());
         b_profil.setCursor(new Cursor(Cursor.HAND_CURSOR));
         menu_bouttons.add(b_profil);
         menu_bouttons.add(b_profil);
+        b_profil.addActionListener(m);
 
         JButton b_baux = new JButton("Mes baux");
         b_baux.setBorderPainted(false);
-        b_baux.setBackground(ENTETE.getCouleur());
+        b_baux.setBackground(Charte.ENTETE.getCouleur());
         b_baux.setCursor(new Cursor(Cursor.HAND_CURSOR));
         menu_bouttons.add(b_baux);
         menu_bouttons.add(b_baux);
+        b_baux.addActionListener(m);
 
         JButton b_loca = new JButton("Locataires");
         b_loca.setBorderPainted(false);
-        b_loca.setBackground(ENTETE.getCouleur());
+        b_loca.setBackground(Charte.ENTETE.getCouleur());
         b_loca.setCursor(new Cursor(Cursor.HAND_CURSOR));
         menu_bouttons.add(b_loca);
         menu_bouttons.add(b_loca);
+        b_loca.addActionListener(m);
 
         JButton b_biens = new JButton("Mes Biens");
         b_biens.setBorderPainted(false);
-        b_biens.setBackground(ENTETE.getCouleur());
+        b_biens.setBackground(Charte.ENTETE.getCouleur());
         b_biens.setCursor(new Cursor(Cursor.HAND_CURSOR));
         menu_bouttons.add(b_biens);
         menu_bouttons.add(b_biens);
@@ -239,7 +244,27 @@ public class PageNouveauBail {
         bas_de_page.add(valider, BorderLayout.EAST);
 
         btn_ajouter_locataire.addActionListener(modele.getAjouterLocataire());
+        this.frame.addComponentListener(new ComponentAdapter() {
+            @Override
+            public void componentResized(ComponentEvent e) {
+                ResizedImage res = new ResizedImage();
+                res.resizeImage("logo+nom.png", PageNouveauBail.this.frame,
+                        PageNouveauBail
+                                .this.logo, 3, 8);
+                int frameWidth = PageNouveauBail.this.frame.getWidth();
+                int frameHeight = PageNouveauBail.this.frame.getHeight();
 
+                int newFontSize = Math.min(frameWidth, frameHeight) / 30;
+
+                // Appliquer la nouvelle police au bouton
+                Font resizedFont = new Font("Arial", Font.PLAIN, newFontSize);
+                b_loca.setFont(resizedFont);
+                b_baux.setFont(resizedFont);
+                b_accueil.setFont(resizedFont);
+                b_profil.setFont(resizedFont);
+                b_biens.setFont(resizedFont);
+            }
+        });
     }
 
     public JFrame getFrame() {
