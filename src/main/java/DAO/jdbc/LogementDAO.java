698eba7c2d37d08ea47069e8f92a9433c698db80
Salairedesalledebain <anonymat2789@gmail.com>
2024-12-02 08:52:20 +0100

    LogemenDAO

--- src/main/java/DAO/jdbc/LogementDAO.java
+++ src/main/java/DAO/jdbc/LogementDAO.java
@@ -2,9 +2,12 @@ package DAO.jdbc;
 
 import DAO.DAOException;
 import DAO.db.ConnectionDB;
+import classes.Batiment;
+import classes.Diagnostic;
 import classes.Logement;
 
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.List;
 
@@ -23,7 +26,6 @@ public class LogementDAO implements DAO.LogementDAO {
 			pstmt.setInt(4, appart.getNbPiece());
 			pstmt.setDouble(5, appart.getSurface());
 			pstmt.setInt(6, 1);
-
 			BatimentDAO bat = new BatimentDAO();
 			pstmt.setInt(7, bat.getIdBat(appart.getVille(), appart.getAdresse()));
 			pstmt.executeUpdate();
@@ -36,9 +38,34 @@ public class LogementDAO implements DAO.LogementDAO {
 
 	@Override
 	public Logement read(int id) throws DAOException {
-		// TODO Auto-generated method stub
-		return null;
-	}
+		ConnectionDB cn;
+		Logement l = null;
+		try {
+			cn = new ConnectionDB();
+			String requete = "SELECT numero_fical, complement_adresse, type_logement, Nombre_pieces, Surface, garage_assoc FROM bienlouable WHERE id = ?";
+			PreparedStatement pstmt = cn.getConnection().prepareStatement(requete);
+			pstmt.setInt(1,id);
+			ResultSet rs = pstmt.executeQuery();
+			if (rs.next()){
+				String num_fisc = rs.getString("numero_ficscal");
+				String compl = rs.getString("complement_adresse");
+				Integer type = rs.getInt("type_logement");
+				Integer nb_pieces = rs.getInt("Nombre_pieces");
+				Double surface = rs.getDouble("Surface");
+				Integer garage = rs.getInt("garage_assoc");
+				String ville = new BatimentDAO().readFisc(num_fisc).getVille();
+				String adresse =  new BatimentDAO().readFisc(num_fisc).getAdresse();
+				List<Diagnostic> diags = new DiagnosticDAO().readAllDiag(num_fisc);
+				Boolean haveG = (garage == 1);
+				l = new Logement(nb_pieces,surface,num_fisc,ville,adresse,compl,diags,haveG);
+			}
+			pstmt.close();
+			cn.closeConnection();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return l;
+    }
 
 	@Override
 	public void update(Logement appart) throws DAOException {
