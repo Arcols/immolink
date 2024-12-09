dd666772c1260ffc1660d66801c1dd34bae8b365
Masterking <153213881+MasterkingStudio@users.noreply.github.com>
2024-12-09 08:28:33 +0100

    Correction ancien sprint

--- src/main/java/modele/PageNouveauBienImmobilier.java
+++ src/main/java/modele/PageNouveauBienImmobilier.java
@@ -411,7 +411,7 @@ public class PageNouveauBienImmobilier {
 			public void componentResized(ComponentEvent e) {
                 ResizedImage res = new ResizedImage();
                 res.resizeImage("logo+nom.png", PageNouveauBienImmobilier.this.frame,
-				PageNouveauBienImmobilier.this.logo, 3, 8);
+					PageNouveauBienImmobilier.this.logo, 3, 8);
                 int frameWidth = PageNouveauBienImmobilier.this.frame.getWidth();
 				int frameHeight = PageNouveauBienImmobilier.this.frame.getHeight();
 
